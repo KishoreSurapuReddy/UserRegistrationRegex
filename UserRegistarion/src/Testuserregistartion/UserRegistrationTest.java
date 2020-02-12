edfc6e4d7f06c981a17eca66879e53d336ae0fa3
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-12 09:51:55 +0530

    [kishorereddy] Add. added UserRegistrationTest to src/Testuserregistration

---
+++ UserRegistarion/src/Testuserregistartion/UserRegistrationTest.java
@@ -0,0 +1,18 @@
+package Testuserregistartion;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import userregistration.UserRegistrationForm;
+
+public class UserRegistrationTest {
+    UserRegistrationForm user;
+    @BeforeClass
+    public void init(){
+        user = new UserRegistrationForm();
+    }
+
+    @Test
+    public void validateEMailId(){
+
+    }
+}
