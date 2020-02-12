f8d318079b51052464b717c867fa8fc84b529d21
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-12 11:43:36 +0530

    [kishorereddy] Add. added LastNameValidationTest.java to src/Testuserregistration

--- UserRegistarion/src/Testuserregistartion/LastNameValidationTest.java
+++ UserRegistarion/src/Testuserregistartion/LastNameValidationTest.java
@@ -1,4 +1,35 @@
 package Testuserregistartion;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import userregistration.PatternMatcher;
+
 public class LastNameValidationTest {
+    static PatternMatcher pattern ;
+    String[] lastName = new String[]{"Surapureddy","Abc"};
+    String[] invalidLastName = new String[]{"abc","123Abc","ABC"};
+
+    @BeforeClass
+    public static void init(){
+        pattern = PatternMatcherInstance.getInstance();
+    }
+
+    @Test
+    public void validateLastName(){
+        for (String name : lastName){
+            boolean validate = pattern.validateLastName(name);
+            System.out.println("LastName is valid :"+name+" "+validate);
+            Assert.assertEquals(true,validate);
+        }
+    }
+
+    @Test
+    public void inValidLastName(){
+        for (String name : invalidLastName){
+            boolean validate = pattern.validateLastName(name);
+            System.out.println("LastName is valid :"+name+" "+validate);
+            Assert.assertEquals(false,validate);
+        }
+    }
 }
