93597e7d86a4ec95333c568cd42c05423d927c2e
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-12 15:04:33 +0530

    [kishorereddy] Refactor. added addUserDetails function and commenting to UserRegistrationForm.java in src/userregistration

--- UserRegistarion/src/userregistration/UserRegistrationForm.java
+++ UserRegistarion/src/userregistration/UserRegistrationForm.java
@@ -1,13 +1,48 @@
 package userregistration;
 
+
+import java.util.InputMismatchException;
+
 public class UserRegistrationForm {
     static PatternMatcher pattern = new PatternMatcher();
-    public void addUserDetails(){
+    static UserInputData user = new UserInputData();
 
+    public static void addUserDetails(){
+        String firstName;
+        String lastName;
+        String EMail_id;
+        String userName;
+        String password;
+        String mobileNumber;
+        System.out.println(" first name :");
+        firstName = user.stringInputData();
+        if (pattern.validateFirstName(firstName)){
+            System.out.println(" last name :");
+            lastName = user.stringInputData();
+            if (pattern.validateLastName(lastName)){
+                System.out.println(" User name :");
+                userName = user.stringInputData();
+                if (pattern.validateUserName(userName)){
+                    System.out.println(" EMailId :");
+                    EMail_id = user.stringInputData();
+                    if (pattern.validateEMailId(EMail_id)){
+                        System.out.println(" Password :");
+                        password = user.stringInputData();
+                        if (pattern.validatePassword(password)){
+                            System.out.println(" MobileNumber :");
+                            mobileNumber = user.stringInputData();
+                            if (pattern.validateMobileNumber(mobileNumber)){
+                                UserRegistration user = new UserRegistration(firstName,lastName,userName,EMail_id,password,mobileNumber);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        throw new InputMismatchException("data was invalid");
     }
     public static void main(String[] args){
-        UserRegistration user = new UserRegistration();
-        String mail = "1098887462";
-        System.out.println(pattern.validateMobileNumber(mail));
+        addUserDetails();
     }
 }
