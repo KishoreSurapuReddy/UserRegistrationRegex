a5edfd959f637760d90964270fb13daf2a467370
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-12 11:41:28 +0530

    [kishorereddy] Add. added PatternMatcherInstance.java to src/Testuserregistration

--- UserRegistarion/src/Testuserregistartion/PatternMatcherInstance.java
+++ UserRegistarion/src/Testuserregistartion/PatternMatcherInstance.java
@@ -1,4 +1,10 @@
 package Testuserregistartion;
 
+import userregistration.PatternMatcher;
+
 public class PatternMatcherInstance {
+    private static final PatternMatcher pattern = new PatternMatcher();
+    public static PatternMatcher getInstance(){
+        return pattern;
+    }
 }
