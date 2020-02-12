d7f27517ccc742998b7a40d634058a09df40ed5a
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-12 12:40:33 +0530

    [kishorereddy] Add. added PasswordValidationTest.java to src\Testuserregistration

--- UserRegistarion/src/Testuserregistartion/PasswordValidationTest.java
+++ UserRegistarion/src/Testuserregistartion/PasswordValidationTest.java
@@ -1,4 +1,35 @@
 package Testuserregistartion;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import userregistration.PatternMatcher;
+
 public class PasswordValidationTest {
+    static PatternMatcher pattern ;
+    String[] password = new String[]{"Kisore@1234","kishoRe$123","123&Abc","&%@Abc123"};
+    String[] invalidPassword = new String[]{"abc","ABC123","123_-abC"};
+
+    @BeforeClass
+    public static void init(){
+        pattern = PatternMatcherInstance.getInstance();
+    }
+
+    @Test
+    public void validatePassword(){
+        for(String name : password ){
+            boolean validate = pattern.validatePassword(name);
+            System.out.println("Password is valid :"+name+" "+validate);
+            Assert.assertEquals(true , validate);
+        }
+    }
+
+    @Test
+    public void inValidatePassword(){
+        for(String name : invalidPassword ){
+            boolean validate = pattern.validatePassword(name);
+            System.out.println("Password is valid :"+name+" "+validate);
+            Assert.assertEquals(false , validate);
+        }
+    }
 }
