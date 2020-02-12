d4228e0abd718c3b3b19cc60aea8661e40c31f49
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-12 14:57:24 +0530

    [kishorereddy] Add. added UserInputData.java to src/userregistration

--- UserRegistarion/src/userregistration/UserInputData.java
+++ UserRegistarion/src/userregistration/UserInputData.java
@@ -1,4 +1,19 @@
 package userregistration;
 
+import java.util.Scanner;
+
 public class UserInputData {
+    Scanner scanner = new Scanner(System.in);
+
+    public String stringInputData(){
+        System.out.println("enter the details :");
+        String data = scanner.nextLine();
+        return data;
+    }
+
+    public int intInputData(){
+        System.out.println("enter the details: ");
+        int data = scanner.nextInt();
+        return data;
+    }
 }
