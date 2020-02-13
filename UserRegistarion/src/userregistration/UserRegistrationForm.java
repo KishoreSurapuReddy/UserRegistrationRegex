177916563fb420d1ec009558690afb74c54a3d19
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-13 12:38:06 +0530

    [kishorereddy] Refactor. modified checking user details in addUserDetails function in UserRegistrationForm.java in src/userregistration

--- UserRegistarion/src/userregistration/UserRegistrationForm.java
+++ UserRegistarion/src/userregistration/UserRegistrationForm.java
@@ -1,10 +1,13 @@
-/**UserRegistration class is used to get the user details and validating the user details
+/**
+ * UserRegistration class is used to get the user details and validating the user details
+ *
  * @author kishorereddy
  * @version 1.0
- * @since 11-02-2020
  * @file UserRegistration.java
+ * @since 11-02-2020
  */
 package userregistration;
+
 import java.util.InputMismatchException;
 
 public class UserRegistrationForm {
@@ -12,43 +15,58 @@ public class UserRegistrationForm {
     static UserInputData user = new UserInputData();
 
     //method to get the user details and validating the details
-    public static void addUserDetails(){
+    public static void addUserDetails() {
         String firstName;
         String lastName;
-        String EMail_id;
+        String eMailId;
         String userName;
         String password;
         String mobileNumber;
         System.out.println(" first name :");
         firstName = user.stringInputData();
-        if (pattern.validateFirstName(firstName)){
-            System.out.println(" last name :");
-            lastName = user.stringInputData();
-            if (pattern.validateLastName(lastName)){
-                System.out.println(" User name :");
-                userName = user.stringInputData();
-                if (pattern.validateUserName(userName)){
-                    System.out.println(" EMailId :");
-                    EMail_id = user.stringInputData();
-                    if (pattern.validateEMailId(EMail_id)){
-                        System.out.println(" Password :");
-                        password = user.stringInputData();
-                        if (pattern.validatePassword(password)){
-                            System.out.println(" MobileNumber :");
-                            mobileNumber = user.stringInputData();
-                            if (pattern.validateMobileNumber(mobileNumber)){
-                                UserRegistration user = new UserRegistration(firstName,lastName,userName,EMail_id,password,mobileNumber);
-                                return;
-                            }
-                        }
-                    }
-                }
-            }
+        if (!pattern.validateFirstName(firstName)) {
+            System.out.println("enter valid first name");
+            addUserDetails();
+            return;
         }
-        throw new InputMismatchException("data was invalid");
+        System.out.println(" last name :");
+        lastName = user.stringInputData();
+        if (!pattern.validateLastName(lastName)) {
+            System.out.println("enter valid last name");
+            addUserDetails();
+            return;
+        }
+        System.out.println(" EMailId :");
+        eMailId = user.stringInputData();
+        if (!pattern.validateEMailId(eMailId)) {
+            System.out.println("enter valid eMailId ");
+            addUserDetails();
+            return;
+        }
+        System.out.println(" User name :");
+        userName = user.stringInputData();
+        if (!pattern.validateUserName(userName)) {
+            System.out.println("enter valid user name");
+            addUserDetails();
+            return;
+        }
+        System.out.println(" Password :");
+        password = user.stringInputData();
+        if (!pattern.validatePassword(password)) {
+            System.out.println("enter valid password ");
+            addUserDetails();
+            return;
+        }
+        System.out.println(" MobileNumber :");
+        mobileNumber = user.stringInputData();
+        if (!pattern.validateMobileNumber(mobileNumber)) {
+            System.out.println("enter valid mobileNumber");
+            addUserDetails();
+            return;
+        }
+        UserRegistration user = new UserRegistration(firstName, lastName, userName, eMailId, password, mobileNumber);
     }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
         addUserDetails();
     }
 }
